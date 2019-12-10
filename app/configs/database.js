b229473e13f0c0a3226c6835caba3bd2d9747ab6
Trinh Tan Tai <fxqtai@gmail.com>
2019-12-10 14:25:41 +0700

    ProductDetailPageLayOut

--- app/configs/database.js
+++ app/configs/database.js
@@ -1,29 +1,29 @@
 const mysql = require('mysql');
 
 exports.createConnection = function createConnnection() {
-	return mysql.createConnection({
-		host: 'localhost',
-		port: '3306',
-		user: 'root',
-		password: '0939414836',
-		database: 'mydb'
-	});
+    return mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '123456789',
+        database: 'mydb'
+    });
 };
 
 exports.load = sql => {
-	return new Promise((resole, reject) => {
-		const con = createConnnection();
-		con.connect(err => {
-			if (err) {
-				reject(err);
-			}
-		});
-		con.query(sql, (error, result, fields) => {
-			if (error) {
-				reject(error);
-			}
-			resole(result);
-		});
-		con.end();
-	});
+    return new Promise((resole, reject) => {
+        const con = createConnnection();
+        con.connect(err => {
+            if (err) {
+                reject(err);
+            }
+        });
+        con.query(sql, (error, result, fields) => {
+            if (error) {
+                reject(error);
+            }
+            resole(result);
+        });
+        con.end();
+    });
 };
