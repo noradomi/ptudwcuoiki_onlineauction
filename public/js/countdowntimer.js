83b66ef800f41c1dc71d6805c1664b33ca99f369
PhucVo <fakermsi1999@gmail.com>
2019-12-12 15:02:03 +0700

    +++Xong render homepage từ dữ liệu database

---
+++ public/js/countdowntimer.js
@@ -0,0 +1,37 @@
+function countDownTimer(expriry_date, id) {
+	var days, hours, minutes, seconds; // variables for time units
+
+	var countdown = document.getElementById(id); // get tag element
+
+	getCountdown(expriry_date);
+
+	setInterval(function() {
+		getCountdown(expriry_date);
+	}, 1000);
+
+	function getCountdown(expriry_date) {
+		// find the amount of "seconds" between now and target
+		var current_date = new Date().getTime();
+		var seconds_left =
+			(new Date(expriry_date).getTime() - current_date) / 1000;
+
+		days = pad(parseInt(seconds_left / 86400));
+		seconds_left = seconds_left % 86400;
+
+		hours = pad(parseInt(seconds_left / 3600));
+		seconds_left = seconds_left % 3600;
+
+		minutes = pad(parseInt(seconds_left / 60));
+		seconds = pad(parseInt(seconds_left % 60));
+
+		// format countdown string + set tag value
+		countdown.innerHTML = `<li><span>${days} D</span></li>
+                <li><span>${hours} H</span></li>
+                <li><span>${minutes} M</span></li>
+                <li><span>${seconds} S</span></li>`;
+	}
+
+	function pad(n) {
+		return (n < 10 ? '0' : '') + n;
+	}
+}
