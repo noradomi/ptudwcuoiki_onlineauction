ecfefdc0294c1639dc48d7039eca8b488a812cfb
Hien Han <hienhan0901@gmail.com>
2019-12-29 05:20:22 +0700

    Xong phần 4.1, 4.2

--- app/routes/admin.route.js
+++ app/routes/admin.route.js
@@ -1,6 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const admincontroller = require('../controllers/admin.controller');
-router.get('/', admincontroller.dasboard);
+const admincontroller = require("../controllers/admin.controller");
+
+router.get("/", (req, res) => {
+  res.redirect("/admin/category");
+});
+router.get("/category", admincontroller.category);
+router.get("/product", admincontroller.product);
+
+router.get("/product/delete", admincontroller.deleteProduct);
+
+router.get("/category/add", admincontroller.fillCategory);
+router.post("/category/add", admincontroller.addCategory);
+router.get("/category/update", admincontroller.fillUpdateCategory);
+router.post("/category/update", admincontroller.updateCategory);
+router.get("/category/delete", admincontroller.deleteCategory);
+
+router.get("/category/addPT", admincontroller.fillProductType);
+router.post("/category/addPT", admincontroller.addProductType);
+router.get("/category/updatePT", admincontroller.fillUpdateProductType);
+router.post("/category/updatePT", admincontroller.updateProductType);
+router.get("/category/deletePT", admincontroller.deleteProductType);
+
 module.exports = router;
