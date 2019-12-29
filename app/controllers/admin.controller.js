ae708cc33d6b0719bdb0f2cdcf6a06d0c610115d
Hien Han <hienhan0901@gmail.com>
2019-12-29 19:05:04 +0700

    update 4.1

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -5,7 +5,6 @@ module.exports.category = async function(req, res) {
         let Cats = await models.category.categoriesAndChild();
         let PTs = await models.product_type.findAllProT();
         res.render("web/admin-category", {
-            user: [req.user],
             layout: "admin-main.hbs",
             title: "Categories management",
             Cats: Cats,
@@ -22,8 +21,7 @@ module.exports.product = async(req, res) => {
         res.render("web/admin-product", {
             layout: "admin-main.hbs",
             title: "Products management",
-            Products: Products,
-            user: [req.user]
+            Products: Products
         });
     } else {
         res.redirect("/");
@@ -41,8 +39,7 @@ module.exports.fillCategory = (req, res) => {
     if (req.user.role === 2) {
         res.render("web/admin-fillCategory", {
             layout: "admin-main.hbs",
-            title: "Categories management",
-            user: [req.user],
+            title: "Categories management"
         });
     } else {
         res.redirect("/");
@@ -54,12 +51,13 @@ module.exports.addCategory = async(req, res, next) => {
     res.redirect("/admin/category/");
 };
 
-module.exports.fillUpdateCategory = (req, res) => {
+module.exports.fillUpdateCategory = async (req, res) => {
     if (req.user.role === 2) {
+        const C = await models.category.findById(req.query.id);
         res.render("web/admin-fillCategory", {
             layout: "admin-main.hbs",
             title: "Categories management",
-            user: [req.user],
+            C: C
         });
     } else {
         res.redirect("/");
@@ -78,12 +76,13 @@ module.exports.deleteCategory = async(req, res, next) => {
 
 //
 
-module.exports.fillProductType = (req, res) => {
+module.exports.fillProductType = async (req, res) => {
     if (req.user.role === 2) {
-        res.render("web/admin-fillCategory", {
+        const Cats = await models.category.categoriesAndChild();
+        res.render("web/admin-fillProductType", {
             layout: "admin-main.hbs",
             title: "Categories management",
-            user: [req.user],
+            Cats: Cats
         });
     } else {
         res.redirect("/");
@@ -91,16 +90,20 @@ module.exports.fillProductType = (req, res) => {
 };
 
 module.exports.addProductType = async(req, res, next) => {
-    await models.product_type.add(req.body.cat_name);
+    await models.product_type.add(req.body.pt_name, req.body.cat_id);
     res.redirect("/admin/category/");
 };
 
-module.exports.fillUpdateProductType = (req, res) => {
+module.exports.fillUpdateProductType = async (req, res) => {
     if (req.user.role === 2) {
-        res.render("web/admin-fillCategory", {
+        const Cats = await models.category.categoriesAndChild();
+        const PT = await models.product_type.findById(req.query.id);
+        console.log(PT);
+        res.render("web/admin-fillProductType", {
             layout: "admin-main.hbs",
             title: "Categories management",
-            user: [req.user],
+            PT: PT,
+            Cats: Cats
         });
     } else {
         res.redirect("/");
@@ -108,7 +111,7 @@ module.exports.fillUpdateProductType = (req, res) => {
 };
 
 module.exports.updateProductType = async(req, res, next) => {
-    await models.product_type.update(req.query.id, req.body.cat_name);
+    await models.product_type.update(req.query.id, req.body.pt_name, req.body.cat_id);
     res.redirect("/admin/category/");
 };
 
@@ -116,28 +119,4 @@ module.exports.deleteProductType = async(req, res, next) => {
 
     await models.product_type.delete(req.query.id);
     res.redirect("/admin/category/");
-};
-module.exports.findAllBidder = async(req, res, next) => {
-    let Bidder = await models.bid_details.findAllBidder();
-    res.render("web/admin-bidder", {
-        layout: "admin-main.hbs",
-        title: "Bidder management",
-        user: [req.user],
-        Bidder: Bidder
-    });
-}
-
-module.exports.findAllUserUpgrade = async(req, res, next) => {
-    let UserUpgrade = await models.user.findUserUpgrade();
-    res.render("web/admin-userupgrade", {
-        layout: "admin-main.hbs",
-        title: "USER WANT TO UPGRADE",
-        user: [req.user],
-        UserUpgrade: UserUpgrade
-    });
-}
-
-module.exports.upbiddertoseller = async(req, res, next) => {
-    await models.user.upgradeBidderToSeller(req.query.id);
-    res.redirect("/admin/userupgrade/");
 };
